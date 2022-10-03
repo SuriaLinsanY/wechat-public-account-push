412dc09a3c7951454f34fc1ea411fcbbde650433
SuriaLinsanY <114908878+SuriaLinsanY@users.noreply.github.com>
2022-10-03 17:22:08 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -23,7 +23,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'ocFxs5mOMZPk3qG53OMjAX9zYFS0',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'aqz9dEym1N106BwyIbmQBil_d9Kke5n0mlQelBO46Ds',
+      useTemplateId: 's0JmP7JbzH3oraOFHrIgU3X3iQE-MmpBmjB8qlhiMEQ',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '04-06',
       festivals: [
